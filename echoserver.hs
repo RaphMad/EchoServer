c97e90fa5a9f20c4f837dcdb480699bf87b0b174
Raphael Mader <RaphMad@users.noreply.github.com>
2015-11-12 15:21:33 +0100

    Use port from commandline if present

--- echoserver.hs
+++ echoserver.hs
@@ -1,16 +1,22 @@
 import Control.Concurrent
 import Control.Monad
-import Data.ByteString.Char8
+import Data.ByteString.Char8 hiding(head, null)
 import Network hiding (accept)
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString
+import System.Environment
 
 main :: IO ()
 main = withSocketsDo $ do
-    -- create TCP socket at port 4242
-    serverSocket <- listenOn $ PortNumber 4242
+    -- create TCP socket at port supplied port or 4242 if no argument was supplied
+    args <- getArgs
+    serverSocket <- listenOn . PortNumber $ args `headOr` 4242
     -- accept new clients in an endless loop
     forever $ acceptClient serverSocket
+    where
+      headOr :: [String] -> Int -> PortNumber
+      headOr list orValue = fromIntegral (if null list then orValue else read $ head list)
+
 
 acceptClient :: Socket -> IO ()
 acceptClient serverSocket = do
@@ -21,9 +27,9 @@ acceptClient serverSocket = do
     void . forkIO $ clientHandler
 
 echo :: Socket -> IO ()
-echo socket = do
+echo sock = do
    -- read chunk of max 4096 bytes
-   message <- recv socket 4096
+   message <- recv sock 4096
    -- respond with 'echo: ' prefix
    let response = pack "echo: " `append` message
-   void $ sendAll socket response
+   void $ sendAll sock response
